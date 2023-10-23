4ab97df8c3ddb7666e2c7b29f8942900c762fe7f
Thiago Marques <thiagossmarques@gmail.com>
2023-10-23 07:04:52 -0300

    feat: insere configurações spring initalizr

---
+++ src/test/java/com/thiagomarques/judotechniques/JudoTechniquesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thiagomarques.judotechniques;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JudoTechniquesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
